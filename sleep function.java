cc50e23ec8a658c4c42a3b12a358ee621202853b
letsdoit11 <100862273+letsdoit11@users.noreply.github.com>
2023-06-15 08:32:07 +0530

    Update sleep function.java

--- sleep function.java
+++ sleep function.java
@@ -1,15 +1,4 @@
-class demoThread extends Thread implements Runnable {
-     public void run() {
-         int i = 5;
-         while (i != 5) {
-             try {
-                 System.out.println(Thread.currentThread().getName());
-                 sleep(2000);
-             } catch (Exception e) {
-                 System.out.println(e);
-             }
-             i++;
-         }
+
 class demo_Sleep extends Thread{
     public void run(){
         for(int i =1;i<5;i++){
