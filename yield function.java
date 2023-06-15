7fa94a209809724a3321b00c5c090ab3fe0ba34c
letsdoit11 <100862273+letsdoit11@users.noreply.github.com>
2023-06-15 08:53:54 +0530

    Add files via upload

---
+++ yield function.java
@@ -0,0 +1,25 @@
+import com.sun.source.tree.YieldTree;
+import java.util.*;
+import java.util.*;
+class yieldthread extends Thread{
+    public void run(){
+        for (int i =0;i<3;i++){
+            System.out.println(Thread.currentThread().getName() + " in controll");
+        }
+    }
+}
+class Main {
+    public static void main(String args[]){
+        yieldthread t1  = new yieldthread();
+        yieldthread t2 = new yieldthread();
+
+        t1.start();
+        t2.start();
+
+for(int i=0;i<3;i++){
+    t1.yield();
+    System.out.println(Thread.currentThread().getName() + " in controll");
+}
+    }
+
+}
