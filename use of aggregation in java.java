894d93e445936a09bbc658daa8e7a4b29608509a
letsdoit11 <100862273+letsdoit11@users.noreply.github.com>
2023-06-19 19:20:53 +0530

    Update use of aggregation in java.java

--- use of aggregation in java.java
+++ use of aggregation in java.java
@@ -11,3 +11,38 @@ class Address{
                    this.country = country;
        }
    }
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+   // make second class as emp
+
+public class Emp {
+
+        int ID;
+        String Name;
+        // Address is class
+        Address address;
+
+        // create constructor
+        public Emp(int ID,String Name,Address address){
+            this.ID = ID;
+            this.Name = Name;
+            this.address = address;
+        }
+        public void display(){
+            System.out.println(ID+" "+Name);
+            System.out.println(address.city +" "+ address.state +" "+ address.country);
+        }
+
+
+        public static void main(String[] args) {
+
+            Address A1 = new Address("Pathankot","pubjab","India");
+            Address A2 = new Address("xyz2","up","India");
+
+            Emp e1 = new Emp(1,"Deepak", A1);
+            Emp e2 = new Emp(2,"Divyam",A2);
+
+            e1.display();
+            e2.display();
+
+        }
+    }
