0cd6f3f64c82f7f5fc4f3e4b9ad1900f396fc75b
letsdoit11 <100862273+letsdoit11@users.noreply.github.com>
2023-06-19 19:19:40 +0530

    Add files via upload

---
+++ use of aggregation in java.java
@@ -0,0 +1,13 @@
+// make first class as Address
+class Address{
+     String city;
+     String state;
+     String country;
+
+    // create constructor
+       public Address(String city,String state,String country){
+                   this.city = city;
+                   this.state = state;
+                   this.country = country;
+       }
+   }
