e225e6f7a20357501e8fdeb4eb5617ea5658678e
letsdoit11 <100862273+letsdoit11@users.noreply.github.com>
2023-06-21 10:34:30 +0530

    Update wrapper class in java.java

--- wrapper class in java.java
+++ wrapper class in java.java
@@ -1,6 +1,8 @@
+// to convert the primitive type to wrapper object
+
 class Main{
    public static void main(String args[]){
-      // to convert the primitive data type to wrapper obj
+      
         int a = 5;
         double g = 6.554;
 
@@ -8,11 +10,32 @@ class Main{
         Double gobj = Double.valueOf(g);
 
         if(aobj instanceof Integer){
+           
             // instance of basically an operator which checks that either object belong to It's class or not
+           
             System.out.println("An object of integer is created:");
         }
         if(gobj instanceof Double){
             System.out.println("An object of double is created:");
         }
+    }
+}
+
+------------------------------------------------------------------------------------------------------------------------------------
+// To convert the wrapper object   to primitive type:
+
+    class Main{
+   public static void main(String args[]){
+
+      
+       Integer aobj = Integer.valueOf(5);
+       Double bobj = Double.valueOf(4.5);
+
+       int a = aobj.intValue();  // using corresponding method:(intValue())
+       double b = bobj.doubleValue();
+
+       System.out.println("a = " + a);
+       System.out.println("b = " + b);
+
     }
 }
